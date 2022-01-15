8fdfaf72e2deab7ff9d94ea37b3ee76d0fd1306d
woohm402 <woohm404@gmail.com>
2022-01-15 16:28:35 +0900

    [#39] :sparkles: project page components 로 옮기기 + 배너 적용

---
+++ src/components/project/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Banner from 'components/common/banner/banner';
+import Layout from 'components/common/layout';
+import Seo from 'components/common/seo';
+import Gatgu from 'components/project/gatgu/gatgu';
+import Guam from 'components/project/guam/guam';
+import * as styles from 'components/project/index.module.scss';
+import Siksha from 'components/project/siksha/siksha';
+import Snuboard from 'components/project/snuboard/snuboard';
+import Snutt from 'components/project/snutt/snutt';
+
+const Project = (): JSX.Element => {
+  return (
+    <Layout>
+      <Seo title="Project" />
+      <main className={styles.wrapper}>
+        <Banner className={styles.banner} />
+        <Snutt />
+        <Siksha />
+        <Snuboard />
+        <Guam />
+        <Gatgu />
+      </main>
+    </Layout>
+  );
+};
+
+export default Project;
--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -2,23 +2,13 @@ import React from 'react';
 
 import Layout from 'components/common/layout';
 import Seo from 'components/common/seo';
-import Gatgu from 'components/project/gatgu/gatgu';
-import Guam from 'components/project/guam/guam';
-import Siksha from 'components/project/siksha/siksha';
-import Snuboard from 'components/project/snuboard/snuboard';
-import Snutt from 'components/project/snutt/snutt';
+import Project from 'components/project';
 
 const ProjectPage = (): JSX.Element => {
   return (
     <Layout>
       <Seo title="Project" />
-      <div style={{ height: '100vh', scrollSnapType: 'y mandatory', overflowY: 'scroll' }}>
-        <Snutt />
-        <Siksha />
-        <Snuboard />
-        <Guam />
-        <Gatgu />
-      </div>
+      <Project />
     </Layout>
   );
 };
