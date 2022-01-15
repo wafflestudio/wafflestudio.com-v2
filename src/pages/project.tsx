e87ead583ca60f17f302159ecc70f3edd48a04c9
woohm402 <woohm404@gmail.com>
2022-01-15 11:36:04 +0900

    [#39] :sparkles: 프로젝트 화면 구성

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,13 +1,26 @@
-import * as React from 'react';
+import React from 'react';
 
 import Layout from 'components/common/layout';
 import Seo from 'components/common/seo';
+import Gatgu from 'components/project/gatgu/gatgu';
+import Guam from 'components/project/guam/guam';
+import Siksha from 'components/project/siksha/siksha';
+import Snuboard from 'components/project/snuboard/snuboard';
+import Snutt from 'components/project/snutt/snutt';
 
-const ProjectPage = (): JSX.Element => (
-  <Layout>
-    <Seo title="Project" />
-    <h1>프로젝트 페이지</h1>
-  </Layout>
-);
+const ProjectPage = (): JSX.Element => {
+  return (
+    <Layout>
+      <Seo title="Project" />
+      <div style={{ height: '100vh', scrollSnapType: 'y mandatory', overflowY: 'scroll' }}>
+        <Snutt />
+        <Siksha />
+        <Snuboard />
+        <Guam />
+        <Gatgu />
+      </div>
+    </Layout>
+  );
+};
 
 export default ProjectPage;
